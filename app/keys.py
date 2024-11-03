41748dc59389bb9ecab0eb9a99d10b1167f33b38
Apple <apple@Apples-MacBook-Pro.local>
2024-11-03 20:05:03 +0530

    changes in workflow

--- app/keys.py
+++ app/keys.py
@@ -4,7 +4,8 @@ import os
 load_dotenv()
 
 REPLICATE_TOKEN=os.environ.get('REPLICATE_ACCESS_KEY') 
-REPLICATE_URL = os.environ.get('REPLICATE_API_URL')
+# REPLICATE_URL = os.environ.get('REPLICATE_API_URL')
+REPLICATE_URL = "https://api.replicate.com/v1/predictions"
 
 REPLICATE_IMAGE_GENERATE_MODEL_VERSION=os.environ.get('REPLICATE_IMAGE_GENERATE_MODEL_VERSION') # -> model name : lucataco/realistic-vision-v5
 REPLICATE_FINE_TUNE_MODEL_VERSION=os.environ.get('REPLICATE_FINE_TUNE_MODEL_VERSION') # -> model name : halimalrasihi/flux-mystic-animals
